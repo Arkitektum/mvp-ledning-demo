c41306330b6938e0affb84e0ad0a3472e15bb2b2
Benjamin Dehli <superelg@gmail.com>
2022-12-09 14:24:04 +0100

    Add SFKB-Samlebase

--- script.js
+++ script.js
@@ -21,18 +21,35 @@ const activateElement = (arrowElement, startTime, endTime) => {
     }
 };
 
-const activateFkbMapElement = (many = false, startTime) => {
-    const fkb_default = document.getElementById("fkb_default");
-    const fkb_results_single = document.getElementById("fkb_results_single");
-    const fkb_results_many = document.getElementById("fkb_results_many");
+const activateSfkbFkbMapElement = (many = false, startTime) => {
+    const sfkbFkb_default = document.getElementById("sfkb-fkb_default");
+    const sfkbFkb_results_single = document.getElementById("sfkb-fkb_results_single");
+    const sfkbFkb_results_many = document.getElementById("sfkb-fkb_results_many");
 
     if (startTime) {
         setTimeout(() => {
-            fkb_default.classList.remove("active");
+            sfkbFkb_default.classList.remove("active");
             if (many) {
-                fkb_results_many.classList.add("active");
+                sfkbFkb_results_many.classList.add("active");
             } else {
-                fkb_results_single.classList.add("active");
+                sfkbFkb_results_single.classList.add("active");
+            }
+        }, startTime);
+    }
+};
+
+const activateSfkbSamlebaseMapElement = (many = false, startTime) => {
+    const sfkbSamlebase_default = document.getElementById("sfkb-samlebase_default");
+    const sfkbSamlebase_results_single = document.getElementById("sfkb-samlebase_results_single");
+    const sfkbSamlebase_results_many = document.getElementById("sfkb-samlebase_results_many");
+
+    if (startTime) {
+        setTimeout(() => {
+            sfkbSamlebase_default.classList.remove("active");
+            if (many) {
+                sfkbSamlebase_results_many.classList.add("active");
+            } else {
+                sfkbSamlebase_results_single.classList.add("active");
             }
         }, startTime);
     }
@@ -56,10 +73,13 @@ const runSimulatedRequests = (many = false) => {
     const fellesApiBox = document.getElementById("fellesApi");
     const arrowToKoeordningForObjekter = document.getElementById("arrowToKoeordningForObjekter");
     const koeordningForObjekter = document.getElementById("koeordningForObjekter");
+    const arrowToSfkbFkb = document.getElementById("arrowToSfkbFkb");
+    const sfkbFkb = document.getElementById("sfkb-fkb");
+    const arrowToSfkbSamlebase = document.getElementById("arrowToSfkbSamlebase");
+    const sfkbSamlebase = document.getElementById("sfkb-samlebase");
     const arrowToEkomportalen = document.getElementById("arrowToEkomportalen");
     const ekomportalen = document.getElementById("ekomportalen");
-    const arrowFkb = document.getElementById("arrowToFkb");
-    const fkb = document.getElementById("fkb");
+    
 
     openDialogButton.innerText = many ? "Mange objekter er valgt" : "Ett objekt er valgt";
     activateElement(openDialogButton, 1);
@@ -69,20 +89,32 @@ const runSimulatedRequests = (many = false) => {
     activateElement(arrowToKoeordningForObjekter, 1900, 3800);
     activateElement(koeordningForObjekter, 3800);
 
-    let fkbEndTime;
+    let sfkbFkbEndTime;
+    let sfkbSamlebaseEndTime;
     let ekomportalenEndTime;
 
-    if (Math.random() < 0.5) {
-        fkbEndTime = 5700;
+    if (Math.random() < 1/3) {
+        sfkbFkbEndTime = 5700;
+        sfkbSamlebaseEndTime = 6300;
         ekomportalenEndTime = 6900;
-    } else {
-        fkbEndTime = 6900;
+    } else if (Math.random() < 2/3) {
+        sfkbFkbEndTime = 6300;
+        sfkbSamlebaseEndTime = 6900;
         ekomportalenEndTime = 5700;
+    } else {
+        sfkbFkbEndTime = 6900;
+        sfkbSamlebaseEndTime = 5700;
+        ekomportalenEndTime = 6300;
     }
 
-    activateElement(arrowFkb, 3800, fkbEndTime);
-    activateElement(fkb, fkbEndTime);
-    activateFkbMapElement(many, fkbEndTime);
+    activateElement(arrowToSfkbFkb, 3800, sfkbFkbEndTime);
+    activateElement(sfkbFkb, sfkbFkbEndTime);
+    activateSfkbFkbMapElement(many, sfkbFkbEndTime);
+
+    activateElement(arrowToSfkbSamlebase, 3800, sfkbSamlebaseEndTime);
+    activateElement(sfkbSamlebase, sfkbSamlebaseEndTime);
+    activateSfkbSamlebaseMapElement(many, sfkbSamlebaseEndTime);
+
     activateElement(arrowToEkomportalen, 3800, ekomportalenEndTime);
     activateElement(ekomportalen, ekomportalenEndTime);
     activateEkomportalenMapElement(ekomportalenEndTime);
